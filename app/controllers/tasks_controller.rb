38f362459de21db44160996b56d023e3d65eb4ef
Andrew Meisenbacher <andrew.meisenbacher@kellogg.northwestern.edu>
2025-02-25 22:15:06 +0000

    review this stuff later for hw

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,27 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    if session["user_id"] != nil
+
+      @tasks = Task.where({"user_id" => session["user_id"]})
+    else
+
+    end
   end
 
   def create
+    if session["user_id"] != nil
     @task = Task.new
+    @task["user_id"] = session["user_id"]
     @task["description"] = params["description"]
     @task.save
     redirect_to "/tasks"
+    else
+      redirect_to "/login"
+    end
   end
 
   def destroy
-    @task = Task.find_by({ "id" => params["id"] })
+    @task = Task.find_by({ "id" => params["id"], "user_id" => session["user_id"] })
     @task.destroy
     redirect_to "/tasks"
   end
